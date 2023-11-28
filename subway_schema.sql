d1916f6a61a1a9486c793b0dea28dc4425cd8b1f
batg099 <109592365+batg099@users.noreply.github.com>
2023-11-28 21:14:10 +0100

    Add files via upload

---
+++ subway_schema.sql
@@ -0,0 +1,12 @@
+create table subway(
+    
+from_stop_I numeric(10,6), 
+to_stop_I numeric(10,6),
+d numeric(10,6),
+duration_avg numeric(10,6),
+n_vehicles numeric(10,6),
+route_I_counts text,
+counts text[]
+
+
+)
